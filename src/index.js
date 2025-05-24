a86466cf7542b938c3f576dfdd37bebc3b8318a9
Sergio Cuervo <sergiocuervo2012@gmail.com>
2025-05-23 19:01:08 -0500

    Cambio de enrutamiento de la app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/mi-proyecto-eventos"> 
+    <BrowserRouter basename="/mi-agenda-eventos"> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
